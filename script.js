c903e8588d290a227341259c30cb863d3a6e2938
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-04-07 11:12:16 +0200

    game logic

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 'use strict';
-// var querySelector = document.querySelector('.message').textContent;
-// var getElement = document.getElementById('titre').textContent;
 
-// document.querySelector('.message').textContent = 'Correct Number 😃';
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 20;
-
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
 var score = 20;
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
 document.querySelector('.score').textContent = score;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
   if (!guess) {
-    document.querySelector('.message').textContent = '😡 No Number ! ';
+    document.querySelector('.message').textContent =
+      " Ceci n'est pas une pipe ! 😡 ";
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number 😃';
+    document.querySelector('.message').textContent =
+      'Tu as trouvé le nombre secret 😃';
+    score += 5;
+    document.querySelector('.score').textContent = score;
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = ' Trop haut ! ⤴️';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = ' Trop bas ! ⤵️';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
-
-  
 });
