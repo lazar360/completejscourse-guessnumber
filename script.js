fcd44d4a1c7a5be3f8a163829cd1ff1fda4b36ba
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-04-07 16:01:14 +0200

    add

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 'use strict';
-let score = 20;
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
+var score = 20;
+var secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.score').textContent = score;
+
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
@@ -9,10 +11,11 @@ document.querySelector('.check').addEventListener('click', function () {
       " Ceci n'est pas une pipe ! 😡 ";
     score--;
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent =
-      'Tu as trouvé le nombre secret 😃';
-    document.querySelector('.score').textContent = score;
-    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'Tu as trouvé le nombre secret 😃';
+    document.querySelector('.number').textContent = guess;
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = ' Trop haut ! ⤴️';
@@ -30,4 +33,13 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 
-
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = ' Start gessing ...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = ' ? ';
+  document.querySelector('.guess').value = ' ';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
