5353b49bcc4f269ececa4a38cb533c96ebde71d3
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-04-07 10:33:42 +0200

    game logic

--- script.js
+++ script.js
@@ -8,12 +8,19 @@
 
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
+var score = 20;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+document.querySelector('.score').textContent = score;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '😡 No Number ! ';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number 😃';
   }
+
+  
 });
