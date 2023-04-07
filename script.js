970a542a69591343e265a02461a14e41ae415efa
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-04-07 09:58:04 +0200

    add

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 'use strict';
-var querySelector = document.querySelector('.message').textContent;
-var getElement = document.getElementById('titre').textContent;
+// var querySelector = document.querySelector('.message').textContent;
+// var getElement = document.getElementById('titre').textContent;
 
-document.querySelector('.message').textContent = 'Correct Number 😃';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
+// document.querySelector('.message').textContent = 'Correct Number 😃';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value); 
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = document.querySelector('.guess').value;
+  console.log(guess);
+});
