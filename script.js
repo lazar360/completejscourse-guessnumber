4fd315053b04b794ef2eb8dba557577a9dea8959
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-04-07 10:05:35 +0200

    add

--- script.js
+++ script.js
@@ -10,6 +10,10 @@
 // console.log(document.querySelector('.guess').value);
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = document.querySelector('.guess').value;
-  console.log(guess);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '😡 No Number ! ';
+  }
 });
